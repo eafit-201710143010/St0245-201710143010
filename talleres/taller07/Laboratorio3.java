3dd5981b96861194daa426f4c5a1c68f393ec225
eafit-201710143010 <30332010+eafit-201710143010@users.noreply.github.com>
2017-09-18 22:11:42 -0500

    Add files via upload

--- talleres/taller07/Laboratorio3.java
+++ talleres/taller07/Laboratorio3.java
@@ -2,25 +2,37 @@
 public class Laboratorio3 {
 public static void main(String[] args)
 {
-	
-	LinkedListMauricio list = new LinkedListMauricio();
-	list.insert(5, 0);
-	list.insert(4, 0);
-	list.insert(3, 0);
-	list.insert(2, 0);
-	list.insert(1, 0);
-	for (int i = 0; i < list.size(); i++)
-	{
-		System.out.println(list.get(i));
-	}
-	
-	System.out.println(list.contains(3));
-	list.remove(2);
-	System.out.println(list.contains(3));
-	for (int i = 0; i < list.size(); i++)
-	{
-		System.out.println(list.get(i));
-	}
+    
+    LinkedListMauricio list = new LinkedListMauricio();
+    list.insert(5, 0);
+    list.insert(4, 0);
+    list.insert(3, 0);
+    list.insert(2, 0);
+    list.insert(1, 0);
+    for (int i = 0; i < list.size(); i++)
+    {
+        System.out.println(list.get(i));
+    }
+    
+    System.out.println(list.contains(3));
+    list.remove(2);
+    System.out.println(list.contains(3));
+    for (int i = 0; i < list.size(); i++)
+    {
+        System.out.println(list.get(i));
+   }
 }
-	
+
+public static int mayorList(LinkedListMauricio a){
+    int r = a.first.data;
+    Node aux = a.first;
+    for(int i=1; i<=a.size-1; i++){
+        if(aux.data>r){
+            r= aux.data;
+        }
+        aux = aux.next;
+    }
+    return r;
+}
+    
 }
